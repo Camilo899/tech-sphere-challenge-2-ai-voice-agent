ef9e9e844f8158562fcadecc80ab5da5614095ac
Cristian Julio <cmejia8989@gmail.com>
2026-08-05 17:50:06 -0500

    feat(api): add health check endpoint

--- apps/api/app/presentation/api/router.py
+++ apps/api/app/presentation/api/router.py
@@ -3,7 +3,12 @@ from fastapi import APIRouter
 from app.presentation.api.routes.follow_up import (
     router as follow_up_router,
 )
+from app.presentation.api.routes.health import (
+    router as health_router,
+)
 
 router = APIRouter()
 
 router.include_router(follow_up_router)
+
+router.include_router(health_router)
